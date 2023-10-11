61380a47219412c5f09b10c8c30cd6134cfa30d8
1Kostik <sorochankostya@gmail.com>
2023-10-11 23:30:03 +0300

    up

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/car_rental_app">
+      <BrowserRouter basename="/car-rental-app">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
