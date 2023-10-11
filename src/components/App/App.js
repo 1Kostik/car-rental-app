81b74b28c58df40d3fac549415c11178f81af864
1Kostik <sorochankostya@gmail.com>
2023-10-11 22:54:13 +0300

    up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
-import Layout from '../Layout/Layout';
-
-const Home = lazy(() => import("../../pages/Home/Home"));
-const Catalog = lazy(() => import("../../pages/Catalog/Catalog"));
-const Favorites = lazy(() => import("../../pages/Favorites/Favorites"));
+import { Route, Routes } from "react-router-dom";
+// import { lazy } from 'react';
+import Layout from "../Layout/Layout";
+import Home from "../../pages/Home/Home";
+import Catalog from "../../pages/Catalog/Catalog";
+import Favorites from "../../pages/Favorites/Favorites";
+import { Container } from "./App.styled";
+// const Home = lazy(() => import("../../pages/Home/Home"));
+// const Catalog = lazy(() => import("../../pages/Catalog/Catalog"));
+// const Favorites = lazy(() => import("../../pages/Favorites/Favorites"));
 
 const App = () => {
   return (
+    <Container>
       <Routes>
-        <Route path='/' element={<Layout/>}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='/catalog' element={<Catalog />} />
-          <Route path='/favorites' element={<Favorites />} />
-          <Route path='*' element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
+    </Container>
   );
 };
 
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+margin: auto;
+padding: 20px;
+width: 1200px;
+text-align: center;
+background-color: white;
+`;
